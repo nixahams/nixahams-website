cc13329e548fd69d74ad2fd00c970fb9d6520046
jd0939567@otc.edu <82665108+joshdejeu@users.noreply.github.com>
2022-10-14 20:58:50 -0500

    Contact section added

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -6,7 +6,7 @@
           <div id="title">Welcome To Our <br> New Website!</div>
           <div id="subtext">You might have noticed some drastic changes to our website</div>
           <div>
-            <button type="button" class="btn btn-primary">Take a tour</button>
+            <input id="top_btn" type="button" value="Take a Tour">
           </div>
         </div>
       </div>
@@ -62,6 +62,67 @@
         </div>
       </div>
 
+      <div class="full_page">
+        <img class="page_background" src="../assets/home/contactus.png" alt="">
+        <div id="home_page3">
+          <div id="contact_form">
+            <div id="form_inner">
+              <form id="form">
+                <div id="form_title_parent">
+                  <div id="form_title">Send us a message</div>
+                </div>
+                <div class="input_parent">
+                  <input required placeholder="Your name..." class="input" type="text" id="name" name="name"><br>
+                </div>
+                <div class="input_parent">
+                  <input required placeholder="Your email..." class="input" type="text" id="email" name="email">
+                </div>
+                <div id="textarea_parent">
+                  <textarea required placeholder="Your message..." class="textarea" name="message" id="message" cols="20" rows="5"></textarea>
+                </div>
+                <div id="form_btn_parent">
+                  <input id="form_btn" type="submit" value="Submit">
+                </div>
+              </form>
+            </div>
+          </div>
+          <div id="contact_top">
+            <div id="contact_top_inner">
+              <div id="contact_text">
+                <div id="contact_title">Need to contact us? Fill out our message form.</div>
+                <div id="contact_subtext">We will get back to you ASAP!</div>
+              </div>
+              <div class="contact_visual">
+                <div class="contact_img_parent">
+                  <img class="contact_img" src="../assets/home/phone-call.png" alt="">
+                </div>
+                <div class="contact_data">000-000-0000</div>
+              </div>
+              <div class="contact_visual">
+                <div class="contact_img_parent">
+                  <img class="contact_img" src="../assets/home/email.png" alt="">
+                </div>
+                <div class="contact_data">emaple@gmail.com</div>
+              </div>
+              <div class="contact_visual">
+                <div class="contact_img_parent">
+                  <img class="contact_img" src="../assets/home/location.png" alt="">
+                </div>
+                <div class="contact_data">123 blueberry st.</div>
+              </div>
+              <div class="contact_visual">
+                <div class="contact_img_parent">
+                  <img class="contact_img" src="../assets/home/world.png" alt="">
+                </div>
+                <div class="contact_data">Get in touch!</div>
+              </div>
+            </div>
+
+          </div>
+          <div id="contact_bottom"></div>
+        </div>
+      </div>
+
 
       
     </div>
@@ -81,6 +142,166 @@
   </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+#top_btn{
+  outline: none;
+  background-color: rgba(0,0,0,0.1);
+  border-radius: 50px;
+  width: 150px; height: 60px;
+  color: white;
+  border: 2px solid white;
+  transition: 0.2s ease;
+}
+#top_btn:hover{
+  background-color: rgba(0,0,0,0.2);
+  box-shadow: inset 0px 0px 10px rgba(0,0,0,0.8);
+}
+#top_btn:active{
+  background-color: rgba(0,0,0,0.3);
+  box-shadow: inset 0px 0px 25px rgba(0,0,0,1);
+}
+.input{
+  height: 100%; width: 100%;
+  background-color: rgba(255,255,255,0.5);
+  outline: none;
+  border: 2px dashed black;
+  padding: 0px 10px;
+}
+.input_parent{
+  width: 100%; height: 15%;
+}
+#textarea_parent{
+  height: 25%; width: 100%;
+}
+.textarea{
+  height: 100%; width: 100%;
+  max-height: 100%;
+  min-height: 25%;
+  background-color: rgba(255,255,255,0.5);
+  outline: none;
+  border: 2px dashed black;
+  padding: 10px;
+}
+#form_btn_parent{
+  height: 10%; width: 100%;
+}
+#form_btn{
+  outline: none;
+  border: 0;
+  background-color: black;
+  color: white;
+  font-family: 'Montserrat', sans-serif;
+  font-weight: normal;
+  font-size: 1.2em;
+  height: 100%; width: 100%;
+}
+#form_inner{
+  width: 100%; height: 100%;
+}
+#form{
+  width: 100%; height: 100%;
+  display: flex; flex-direction: column;
+  justify-content: space-around;
+}
+#form_title_parent{
+  width: 100%; height: 10%;
+}
+#form_title{
+  font-family: 'Montserrat', sans-serif;
+  font-weight: normal;
+  font-size: 2em;
+  height: 100%; width: 100%;
+}
+.full_page{
+  width: 100%;height: 110vh;
+  position: relative;
+}
+.page_background{
+  position: absolute;
+  width: 100%; height: 100%;
+  top: 0; left: 0;
+  z-index: 1;
+  object-fit: cover;
+  background-color: rgb(225, 225, 225);
+  object-position: left;
+}
+#home_page3{
+  background-color: transparent;
+  z-index: 2;
+  position: relative;
+  width: 100%;height: 100%;
+}
+#contact_form{
+  position: absolute;
+  z-index: 2;
+  width: 30%; height: 60%;
+  bottom: 10%; right: 10%;
+  background-color: rgb(225, 225, 225);
+  box-shadow: 0px 0px 30px rgba(0, 0, 0,0.5);
+  padding: 4%;
+  color: black;
+  background-image: url('../assets/carosel-imgs/bg1.jpg');
+  background-position: left;
+  background-repeat: no-repeat;
+}
+#contact_top{
+  width: 100%; height: 80%;
+  background-color: transparent;
+  backdrop-filter: blur(20px);
+  color: black;
+  /* top | right | bottom | left */
+  padding:  0vh 45% 10vh 30%;
+  display: flex;
+  align-items: flex-end;
+  justify-content: flex-start;
+}
+#contact_top_inner{
+  position: relative;
+  /* left side of contact info */
+  height: 70%; width: 100%;
+  display: flex; flex-direction: column;
+  justify-content: space-around;
+}
+#contact_text{
+  font-family: 'Montserrat', sans-serif;
+  width: 100%;height: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+#contact_title{
+  font-weight: bold;
+  font-size: 2em;
+}
+#contact_subtext{
+  font-weight: normal;
+  color: rgba(0,0,0,0.7);
+  font-size: 1.5em;
+  display: flex; justify-content: flex-start; align-items: center;
+  height: 40%;
+}
+.contact_visual{
+  display: flex; flex-direction: row;
+  width: 100%; height: 12.5%;
+}
+.contact_img_parent{
+  width: 20%; height: 100%;
+  display: flex;
+  justify-content: center; align-items: center;
+}
+.contact_img{
+  width: 50%; height: 50%;
+  object-fit: contain;
+}
+.contact_data{
+  width: 80%; height: 100%;
+  display: flex;
+  justify-content: flex-start; align-items: center;
+}
+#contact_bottom{
+  width: 100%; height: 20%;
+  background-color: black;
+  color: white;
+}
 .c-inner, .c-item, .c-parent{position: relative;}
 #c-btn, .c-indicator{
   z-index: 2;
@@ -133,7 +354,7 @@
 }
 #subtext{
   font-size: 1.5em;
-  color: rgba(255,255,255,0.5);
+  color: rgba(255,255,255,0.7);
   font-family: 'Montserrat', sans-serif;
   font-weight: normal;
 }
@@ -251,6 +472,33 @@
   #info_btn{
     width: 100%; height: 100%;
   }
+  #contact_top_inner{
+    height: 100%; width: 100%;
+    display: flex; flex-direction: column;
+    justify-content: flex-start; align-items: center;
+  }
+  #contact_top{
+    height: 40%;
+    padding: 0vh 15% 5vh 15%;
+  }
+  #contact_bottom{
+    height: 60%;
+    background-color: black;
+  }
+  #contact_form{
+    width: 80%; height: 50%;
+    bottom: 13%; right: 10%;
+    background-color: rgb(225, 225, 225);
+  }
+  .full_page{
+    width: 100%;height: 150vh;
+  }
+  .contact_img_parent{
+    justify-content: flex-start; align-items: flex-start;
+  }
+  .contact_data{
+    justify-content: flex-start; align-items: flex-start;
+  }
 }
 
 /* Half-Screen Styles */
@@ -278,6 +526,17 @@
     display: flex; flex-direction: column;
     width: 100%; height: 70%;
   }
+  #contact_top{
+    height: 50%;
+    padding: 0vh 10% 5vh 10%;
+  }
+  #contact_bottom{
+    height: 50%;
+    background-color: black;
+  }
+  #contact_form{
+    bottom: 2%;
+  }
 }
 
 
@@ -304,6 +563,10 @@
   #info_big_right{
     text-align: center;
     font-size: 2em;
+  }
+  #contact_top{
+    height: 50%;
+    padding: 0vh 10% 5vh 10%;
   }
 }
 </style>
