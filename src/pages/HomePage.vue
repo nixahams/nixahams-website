4b89a3f570110a69c86793532602f21cbe7d2009
Preston Roesslet <proesslet@gmail.com>
2023-06-17 00:42:59 -0500

    Updated home page text

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,64 +1,105 @@
 <template>
   <div id="home_page">
-
     <div id="home_top">
       <div id="text_image">
-        <div id="title">Welcome To Our <br> New Website!</div>
-        <div id="subtext">
-          Scroll to find more info about our meetings!
+        <div id="title">
+          Welcome To the <br />
+          Nixa Amateur Radio Club!
         </div>
+        <div id="subtext">Scroll to find more info about our club!</div>
       </div>
     </div>
 
     <div id="home-carousel" class="carousel slide" data-bs-ride="true">
       <div class="carousel-indicators c-indicator">
-        <button type="button" data-bs-target="#home-carousel" data-bs-slide-to="0" class="active" aria-current="true"
-          aria-label="Slide 1"></button>
-        <button type="button" data-bs-target="#home-carousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
-        <button type="button" data-bs-target="#home-carousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        <button
+          type="button"
+          data-bs-target="#home-carousel"
+          data-bs-slide-to="0"
+          class="active"
+          aria-current="true"
+          aria-label="Slide 1"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#home-carousel"
+          data-bs-slide-to="1"
+          aria-label="Slide 2"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#home-carousel"
+          data-bs-slide-to="2"
+          aria-label="Slide 3"
+        ></button>
       </div>
       <div class="carousel-inner c-inner">
         <div class="carousel-item c-item active">
-          <img src="https://i.imgur.com/wS0nAgM.jpeg" class="d-block w-100 c-img" alt="Slide 1">
+          <img
+            src="https://i.imgur.com/wS0nAgM.jpeg"
+            class="d-block w-100 c-img"
+            alt="Slide 1"
+          />
         </div>
         <div class="carousel-item c-item">
-          <img src="https://i.imgur.com/DMekFMZ.jpeg" class="d-block w-100 c-img" alt="Slide 2">
+          <img
+            src="https://i.imgur.com/DMekFMZ.jpeg"
+            class="d-block w-100 c-img"
+            alt="Slide 2"
+          />
         </div>
         <div class="carousel-item c-item">
-          <img src="../assets/carosel-imgs/bg3.jpg" class="d-block w-100 c-img" alt="Silde 3">
+          <img
+            src="../assets/carosel-imgs/bg3.jpg"
+            class="d-block w-100 c-img"
+            alt="Silde 3"
+          />
         </div>
       </div>
-      <button id="c-btn" class="carousel-control-prev" type="button" data-bs-target="#home-carousel"
-        data-bs-slide="prev">
+      <button
+        id="c-btn"
+        class="carousel-control-prev"
+        type="button"
+        data-bs-target="#home-carousel"
+        data-bs-slide="prev"
+      >
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Previous</span>
       </button>
-      <button id="c-btn" class="carousel-control-next" type="button" data-bs-target="#home-carousel"
-        data-bs-slide="next">
+      <button
+        id="c-btn"
+        class="carousel-control-next"
+        type="button"
+        data-bs-target="#home-carousel"
+        data-bs-slide="next"
+      >
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Next</span>
       </button>
     </div>
 
     <div id="home_text_image">
-      <img id="home_image" src="../assets/home/fieldday.jpg" alt="">
+      <img id="home_image" src="../assets/home/fieldday.jpg" alt="" />
       <div id="home_text_parent">
         <div id="this_container">
           <div id="home_title">Group Meetings</div>
-          <div id="home_text">Join us for our monthly meetings, click to join our live stream!</div>
+          <div id="home_text">
+            Join us for our monthly meetings, click to join our live stream!
+          </div>
           <div id="home_info">
             <div id="home_info_left">
-              <div id="info_big_left">{{date}}</div>
+              <div id="info_big_left">{{ date }}</div>
               <div id="info_small_left">{{ month }} {{ year }}</div>
             </div>
             <div id="home_info_right">
               <div id="info_big_right">Meeting</div>
-              <div id="info_small_right">{{city}}</div>
+              <div id="info_small_right">{{ city }}</div>
             </div>
           </div>
           <div id="home_btn">
             <a href="/#/meetings">
-              <button id="info_btn">Webinar Link 
+              <button id="info_btn">
+                Webinar Link
                 <i class="fa-solid fa-arrow-right"></i>
               </button>
             </a>
@@ -68,29 +109,54 @@
     </div>
 
     <div class="full_page">
-      <img class="page_background" src="../assets/home/contactus.png" alt="">
+      <img class="page_background" src="../assets/home/contactus.png" alt="" />
       <div id="home_page3">
         <div id="contact_form">
           <!-- temp -->
           <!-- <div id="work_div"></div> -->
           <!-- temp -->
           <div id="form_inner">
-            <form id="form" action="https://formsubmit.co/el/bulixe" method="POST">
+            <form
+              id="form"
+              action="https://formsubmit.co/el/bulixe"
+              method="POST"
+            >
               <div id="form_title_parent">
                 <div id="form_title">Send us a message</div>
               </div>
               <div class="input_parent">
-                <input required placeholder="Your name..." class="input" type="text" id="name" name="name"><br>
+                <input
+                  required
+                  placeholder="Your name..."
+                  class="input"
+                  type="text"
+                  id="name"
+                  name="name"
+                /><br />
               </div>
               <div class="input_parent">
-                <input required placeholder="Your email..." class="input" type="text" id="email" name="email">
+                <input
+                  required
+                  placeholder="Your email..."
+                  class="input"
+                  type="text"
+                  id="email"
+                  name="email"
+                />
               </div>
               <div id="textarea_parent">
-                <textarea required placeholder="Your message..." class="textarea" name="message" id="message" cols="20"
-                  rows="5"></textarea>
+                <textarea
+                  required
+                  placeholder="Your message..."
+                  class="textarea"
+                  name="message"
+                  id="message"
+                  cols="20"
+                  rows="5"
+                ></textarea>
               </div>
               <div id="form_btn_parent">
-                <input id="form_btn" type="submit" value="Submit">
+                <input id="form_btn" type="submit" value="Submit" />
               </div>
             </form>
           </div>
@@ -98,74 +164,71 @@
         <div id="contact_top">
           <div id="contact_top_inner">
             <div id="contact_text">
-              <div id="contact_title">Need to contact us? Fill out our message form.</div>
+              <div id="contact_title">
+                Need to contact us? Fill out our message form.
+              </div>
               <div id="contact_subtext">We will get back to you ASAP!</div>
             </div>
-
           </div>
-
         </div>
         <div id="contact_bottom"></div>
       </div>
     </div>
-
-
-
   </div>
 </template>
-  
+
 <script>
-import axios from 'axios'
+import axios from "axios";
 export default {
-  name: 'HomePage',
-  components: {
-  },
+  name: "HomePage",
+  components: {},
   data() {
     return {
       date: 14,
-      month: 'Jan',
+      month: "Jan",
       year: 2023,
-      city: 'Springfield',
-    }
+      city: "Springfield",
+    };
   },
-  methods:{
-    scrollToTop() {document.body.scrollTop = 0;},
-    getMeetingInfo(VueObj)
-    {
+  methods: {
+    scrollToTop() {
+      document.body.scrollTop = 0;
+    },
+    getMeetingInfo(VueObj) {
       const URL = `https://us-east-1.aws.data.mongodb-api.com/app/app-0-yyrfg/endpoint/meetings/all`;
-            axios.get(URL)
-            .then(function (response) {
-                // handle success
-                if(!response.data){
-                    VueObj.meetingData = {};
-                    return;
-                }
-                let indx = response.data[response.data.length-1].months.length-1;
-                let databit = response.data[response.data.length-1].months[indx];
-                VueObj.date = databit.day;
-                VueObj.month = databit.month;
-                VueObj.year = response.data[response.data.length-1].year;
-                VueObj.city = databit.city;
-            })
-            .catch(function (error) {
-                // handle error
-                VueObj.meetingData = {};
-                error;
-            })
-            .finally(function () {
-                // always executed
-            });
-    }
+      axios
+        .get(URL)
+        .then(function (response) {
+          // handle success
+          if (!response.data) {
+            VueObj.meetingData = {};
+            return;
+          }
+          let indx = response.data[response.data.length - 1].months.length - 1;
+          let databit = response.data[response.data.length - 1].months[indx];
+          VueObj.date = databit.day;
+          VueObj.month = databit.month;
+          VueObj.year = response.data[response.data.length - 1].year;
+          VueObj.city = databit.city;
+        })
+        .catch(function (error) {
+          // handle error
+          VueObj.meetingData = {};
+          error;
+        })
+        .finally(function () {
+          // always executed
+        });
+    },
   },
-  mounted(){
+  mounted() {
     this.scrollToTop();
     this.getMeetingInfo(this);
-  }
-}
+  },
+};
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 .input {
   height: 100%;
   width: 100%;
@@ -174,7 +237,7 @@ export default {
   border: 2px dashed black;
   padding: 0px 20px;
   color: black;
-  font-family: 'Montserrat', sans-serif;
+  font-family: "Montserrat", sans-serif;
   font-weight: bold;
 }
 
@@ -198,7 +261,7 @@ export default {
   border: 2px dashed black;
   padding: 20px;
   color: black;
-  font-family: 'Montserrat', sans-serif;
+  font-family: "Montserrat", sans-serif;
   font-weight: bold;
 }
 
@@ -212,7 +275,7 @@ export default {
   border: 0;
   background-color: black;
   color: white;
-  font-family: 'Montserrat', sans-serif;
+  font-family: "Montserrat", sans-serif;
   font-weight: normal;
   font-size: 1.2em;
   height: 100%;
@@ -238,7 +301,7 @@ export default {
 }
 
 #form_title {
-  font-family: 'Montserrat', sans-serif;
+  font-family: "Montserrat", sans-serif;
   font-weight: normal;
   font-size: 2em;
   height: 100%;
@@ -305,7 +368,7 @@ export default {
   box-shadow: 0px 0px 30px rgba(0, 0, 0, 0.5);
   padding: 4%;
   color: black;
-  background-image: url('../assets/carosel-imgs/bg1.jpg');
+  background-image: url("../assets/carosel-imgs/bg1.jpg");
   background-position: left;
   background-repeat: no-repeat;
 }
@@ -334,7 +397,7 @@ export default {
 }
 
 #contact_text {
-  font-family: 'Montserrat', sans-serif;
+  font-family: "Montserrat", sans-serif;
   width: 100%;
   height: 50%;
   display: flex;
@@ -420,7 +483,7 @@ export default {
   height: 100%;
   width: 100%;
   object-fit: cover;
-  filter: brightness(50%)
+  filter: brightness(50%);
 }
 
 #home_page {
@@ -461,15 +524,15 @@ export default {
 #title {
   font-size: 3em;
   color: white;
-  font-family: 'Poppins', sans-serif;
-  font-family: 'Montserrat', sans-serif;
+  font-family: "Poppins", sans-serif;
+  font-family: "Montserrat", sans-serif;
   font-weight: bold;
 }
 
 #subtext {
   font-size: 1.5em;
   color: rgba(255, 255, 255, 0.7);
-  font-family: 'Montserrat', sans-serif;
+  font-family: "Montserrat", sans-serif;
   font-weight: normal;
 }
 
@@ -481,7 +544,7 @@ export default {
 
 /* Text with image and info */
 #home_text_image {
-  font-family: 'Montserrat', sans-serif;
+  font-family: "Montserrat", sans-serif;
   width: 100%;
   height: 110vh;
   position: relative;
@@ -520,7 +583,7 @@ export default {
   align-items: flex-start;
   flex-direction: column;
 }
-#home_title{
+#home_title {
   font-size: 3em;
   color: white;
   font-weight: bold;
@@ -539,7 +602,7 @@ export default {
   text-transform: uppercase;
 }
 
-#home_info>div {
+#home_info > div {
   border: 3px solid rgb(84, 158, 238);
   color: white;
   width: 100%;
@@ -566,7 +629,7 @@ export default {
 
 #info_small_right,
 #info_small_left {
-  color: rgba(255, 255, 255, 0.7)
+  color: rgba(255, 255, 255, 0.7);
 }
 
 #info_big_left {
@@ -598,7 +661,7 @@ export default {
   outline: none;
   width: 100%;
   height: 100%;
-  font-family: 'Montserrat', sans-serif;
+  font-family: "Montserrat", sans-serif;
   font-weight: bold;
   font-size: 2em;
   background-color: white;
@@ -617,17 +680,16 @@ export default {
 }
 
 @media screen and (max-width: 1300px) {
-  #form_title{
+  #form_title {
     font-size: 1.7em;
   }
 }
-
 
 @media screen and (max-width: 1200px) {
   #home_text {
     text-align: center;
   }
-  #form_title{
+  #form_title {
     font-size: 2.5em;
   }
   #this_container {
@@ -695,7 +757,6 @@ export default {
 
 /* Half-Screen Styles */
 @media screen and (max-width: 900px) {
-
   #contact_subtext {
     font-size: 1.3em;
   }
@@ -703,20 +764,22 @@ export default {
   #home_info_right {
     font-size: 0.8em;
   }
-  #subtext,#home_text {
+  #subtext,
+  #home_text {
     font-size: 1.2em;
   }
   #contact_title {
     font-size: 1.5em;
   }
-  #title,#home_title{
+  #title,
+  #home_title {
     font-size: 2em;
   }
   #this_container {
     height: 50%;
     width: 100%;
   }
-  #form_title{
+  #form_title {
     font-size: 1.7em;
   }
   #home_text_parent {
@@ -763,7 +826,7 @@ export default {
     bottom: 2%;
     height: 50%;
   }
-  #textarea_parent{
+  #textarea_parent {
     font-size: 0.8em;
   }
   .input_parent {
@@ -776,7 +839,6 @@ export default {
   }
 }
 
-
 /* Mobile Styles */
 @media screen and (max-width: 768px) {
   #contact_subtext {
@@ -785,11 +847,13 @@ export default {
   #contact_title {
     font-size: 1.3em;
   }
-  #subtext,#home_text {
+  #subtext,
+  #home_text {
     font-size: 1em;
     text-shadow: 2px 2px 3px black;
   }
-  #title,#home_title {
+  #title,
+  #home_title {
     font-size: 1.5em;
   }
   #this_container {
@@ -834,7 +898,7 @@ export default {
     bottom: 2%;
     height: 35%;
   }
-  #textarea_parent{
+  #textarea_parent {
     font-size: 0.65em;
   }
   .input_parent {
@@ -844,4 +908,3 @@ export default {
   }
 }
 </style>
-  
