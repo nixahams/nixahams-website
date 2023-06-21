02e90c8994122ff3bfd7808319f55c3f50a7156d
new name <dejeujosh@gmail.com>
2023-06-21 16:18:25 -0500

    Vue3 routes updated, page loads no erros

--- src/main.js
+++ src/main.js
@@ -6,10 +6,11 @@ import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
-const app = createApp(App);
 
-app.use(BootstrapVue);
-app.use(IconsPlugin);
-app.use(router);
 
-app.mount("#app");
+createApp(App)
+.use(BootstrapVue)
+.use(IconsPlugin)
+.use(router)
+.mount("#app")
+
