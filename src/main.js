84db319a8ea9aa53a30defec43da7677aa2bd0a9
Preston Roesslet <proesslet@gmail.com>
2023-06-20 23:43:42 -0500

    started Vue3 migration

--- src/main.js
+++ src/main.js
@@ -1,23 +1,22 @@
-import Vue from 'vue';
-import App from './App.vue';
-import VueRouter from 'vue-router';
+import { createApp } from "vue";
+import VueRouter from "vue-router";
 import routes from "./routes";
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
+const app = createApp({});
 
-Vue.use(BootstrapVue);
-Vue.use(IconsPlugin);
-Vue.use(VueRouter);//enables routing
+app.use(BootstrapVue);
+app.use(IconsPlugin);
+app.use(VueRouter); //enables routing
 
 const router = new VueRouter({
-  routes
-});//routes stores in an array of objects
+  routes,
+}); //routes stores in an array of objects
 
-Vue.config.productionTip = false
+app.use(router);
 
-new Vue({
-  router,
-  render: h => h(App),
-}).$mount('#app')
+app.config.productionTip = false;
+
+app.mount("#app");
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// vue.config.js
+module.exports = {
+  chainWebpack: (config) => {
+    config.resolve.alias.set("vue", "@vue/compat");
+
+    config.module
+      .rule("vue")
+      .use("vue-loader")
+      .tap((options) => {
+        return {
+          ...options,
+          compilerOptions: {
+            compatConfig: {
+              MODE: 3,
+            },
+          },
+        };
+      });
+  },
+};
