af0e4794032b513d2251bd0c0dac2cc9f37a3548
new name <dejeujosh@gmail.com>
2023-07-07 17:30:52 -0500

    npm i vite

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from "vue";
-import App from "./App.vue";
+import App from "../App.vue";
 import router from "./router";
 import { createStore } from "vuex";
 import axios from "axios";
