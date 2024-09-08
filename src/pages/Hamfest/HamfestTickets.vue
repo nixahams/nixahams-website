0affe4edca45d4948c682a2d81f8379857040573
Preston Roesslet <proesslet@gmail.com>
2024-09-08 14:17:28 -0500

    updated total table amount

--- src/pages/Hamfest/HamfestTickets.vue
+++ src/pages/Hamfest/HamfestTickets.vue
@@ -128,7 +128,7 @@ export default {
       axios("/stripe/hamfest-tables-sold", {
         method: "GET",
       }).then((response) => {
-        this.tablesRemaining = 32 - response.data;
+        this.tablesRemaining = 40 - response.data;
       });
     },
     changeTicketType(type) {
